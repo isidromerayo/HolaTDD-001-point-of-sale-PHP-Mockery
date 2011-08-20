407fa0f20b9192b0b1814290cdac606b9197cbf0
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2011-08-20 13:52:43 +0200

    Initial commit to project Hola TDD point of sale on PHP with Mockery

---
+++ tests/PointOfSaleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *
+ * @author Isidro Merayo
+ */
+class PointOfSaleTest extends PHPUnit_Framework_TestCase {
+    
+    /**
+     * @test
+     */
+    public function onBarcode_search_catalog() {
+        $this->markTestIncomplete();
+    }
+}
