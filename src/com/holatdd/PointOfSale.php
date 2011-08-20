ab735dba08f75339e0d5774b85e112736be20206
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2011-08-20 19:47:45 +0200

    Second test on GREEN

--- src/com/holatdd/PointOfSale.php
+++ src/com/holatdd/PointOfSale.php
@@ -8,12 +8,14 @@ namespace com\holatdd;
 class PointOfSale {
     
     private $catalog;
+    private $screen;
     
-    public function __construct(Catalog $catalog) {
+    public function __construct(Catalog $catalog, Screen $screen) {
         $this->catalog = $catalog;
+        $this->screen = $screen;
     }
     
     public function onBarcode($barcode) {
-        $this->catalog->search($barcode);
+        return $this->screen->show(($this->catalog->search($barcode)));
     }
 }
--- tests/PointOfSaleTest.php
+++ tests/PointOfSaleTest.php
@@ -19,12 +19,30 @@ class PointOfSaleTest extends PHPUnit_Framework_TestCase {
      * @test
      */
     public function onBarcode_search_catalog() {
+        $screen = m::mock('\com\holatdd\Screen');
         $catalog = m::mock('\com\holatdd\Catalog');
         
         $catalog->shouldReceive('search')->with('123')->once();
+        $screen->shouldReceive('show');
         
-        $pointOfSale = new PointOfSale($catalog);
+        $pointOfSale = new PointOfSale($catalog, $screen);
         $pointOfSale->onBarcode('123');
+        
+    }
+    
+    /**
+     * @test
+     */
+    public function onBarcode_show_price() {
+        $screen = m::mock('\com\holatdd\Screen');
+        $catalog = m::mock('\com\holatdd\Catalog');
+                
+        
+        $screen->shouldReceive('show')->once();
+        $catalog->shouldReceive('search')->with('123')->andReturn('1€');
+        
+        $pointOfSale = new PointOfSale($catalog, $screen);
+        $price = $pointOfSale->onBarcode('123');
         
     }
 }
